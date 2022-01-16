8fe27cb32cbc791f46815929e6dc980d2ba7b8c6
Alexey Natalushko <91388240+GoldRetard@users.noreply.github.com>
2022-01-16 10:36:16 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+  <head>
+      <link rel="stylesheet" media="all" href="//static.zdassets.com/hc/assets/application-79172500fb4dbec2484043d570946543.css" id="stylesheet" />
+      <body class="community-enabled">
+        <form id="new_request" class="request-form" data-form="" data-form-type="request" action="/request" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" />
 
+
+
+
+  
+
+  <div class="form-field select optional request_ticket_form_id"><label for="request_issue_type_select">What can we help you with?</label>
+<select name="request[ticket]" id="request_issue_type_select" aria-label="What can we help you with?" autofocus="autofocus"><option data-url="https://pax.yougro.xyz" value="-">-</option>
+<option data-url="https://pax.yougro.xyz/new?ticket=360000029212" value="360000029212">Help &amp; Support</option>
+<option data-url="https://pax.yougro.xyz/hc/en-us/requests/new?ticket_form_id=360000118612" value="360000118612">Billing</option>
+<option data-url="https://pax.yougro.xyz/hc/en-us/requests/new?ticket_form_id=360000029731" value="360000029731">Trust &amp; Safety</option>
+<option data-url="https://pax.yougro.xyz/hc/en-us/requests/new?ticket_form_id=360000168511" value="360000168511">Community Programs</option>
+<option data-url="https://pax.yougro.xyz/hc/en-us/requests/new?ticket_form_id=360006586013" value="360006586013">Bug Reporting</option>
+<option data-url="https://pax.yougro.xyz/hc/en-us/requests/new?ticket_form_id=360005592534" value="360005592534">Developer Support</option>
+<option data-url="https://pax.yougro.xyz/hc/en-us/requests/new?ticket_form_id=360004944073" value="360004944073">Translation Errors</option>
+<option data-url="https://pax.yougro.xyz/hc/en-us/requests/new?ticket_form_id=360000466272" value="360000466272">File a DMCA Takedown Request</option>
+<option data-url="https://pax.yougro.xyz/hc/en-us/requests/new?ticket_form_id=1500000547262" value="1500000547262">NSFW Server Owner Appeals and Questions</option></select></div>
+
+  </form>
+  </div>
+  </div>
+</html>
